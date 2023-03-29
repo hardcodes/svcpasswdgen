874edaad026908ffc52283f2abcaa9eb1f6456ab
Sven Putze <github@hardcodes.de>
2023-03-29 17:57:44 +0200

    refactoring

--- src/password.rs
+++ src/password.rs
@@ -2,8 +2,8 @@ use crate::cli_parser::CliArguments;
 use ring::digest;
 
 /// build sha512 hash from the given value.
-fn get_hashed(value: &str) -> Vec<u8> {
-    let digest = digest::digest(&digest::SHA512, value.as_bytes());
+fn get_sha512_digest(input: &str) -> Vec<u8> {
+    let digest = digest::digest(&digest::SHA512, input.as_bytes());
     digest.as_ref().to_vec()
 }
 
@@ -11,12 +11,12 @@ fn get_hashed(value: &str) -> Vec<u8> {
 pub fn hash_cli_args(cli_args: &CliArguments) -> Vec<u8> {
     // build the hashes over the input values
     let mut passwd_hashes: Vec<u8> = Vec::new();
-    passwd_hashes.append(&mut get_hashed(&cli_args.machine));
-    passwd_hashes.append(&mut get_hashed(&cli_args.account));
-    passwd_hashes.append(&mut get_hashed(&cli_args.seed));
+    passwd_hashes.append(&mut get_sha512_digest(&cli_args.machine));
+    passwd_hashes.append(&mut get_sha512_digest(&cli_args.account));
+    passwd_hashes.append(&mut get_sha512_digest(&cli_args.seed));
     if cli_args.extra.is_some() {
         for extra in cli_args.extra.as_ref().unwrap() {
-            passwd_hashes.append(&mut get_hashed(extra));
+            passwd_hashes.append(&mut get_sha512_digest(extra));
         }
     }
     passwd_hashes
@@ -24,8 +24,11 @@ pub fn hash_cli_args(cli_args: &CliArguments) -> Vec<u8> {
 
 /// build password, of prefix + result hash + suffix
 pub fn build_password(cli_args: &CliArguments, password_hashes: &Vec<u8>) -> String {
-    let digest = digest::digest(&digest::SHA512, password_hashes);
-    let digest_result: String = digest.as_ref().iter().map(|x| format!("{:02x}", &x)).collect();
+    let digest_result: String = digest::digest(&digest::SHA512, password_hashes)
+        .as_ref()
+        .iter()
+        .map(|x| format!("{:02x}", &x))
+        .collect();
     let mut password = cli_args.prefix.clone();
     let used_hash = &digest_result[..cli_args.length as usize];
     password.push_str(used_hash);
