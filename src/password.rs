f446caf90afe09c862b697174057e1a43af8ec8e
Sven Putze <github@hardcodes.de>
2023-03-29 17:46:49 +0200

    using different crate for sha512 digest

--- src/password.rs
+++ src/password.rs
@@ -1,9 +1,10 @@
 use crate::cli_parser::CliArguments;
-use sha2::{Digest, Sha512};
+use ring::digest;
 
 /// build sha512 hash from the given value.
 fn get_hashed(value: &str) -> Vec<u8> {
-    Sha512::digest(value).to_vec()
+    let digest = digest::digest(&digest::SHA512, value.as_bytes());
+    digest.as_ref().to_vec()
 }
 
 // build sha512 hashes over all input values
@@ -23,12 +24,10 @@ pub fn hash_cli_args(cli_args: &CliArguments) -> Vec<u8> {
 
 /// build password, of prefix + result hash + suffix
 pub fn build_password(cli_args: &CliArguments, password_hashes: &Vec<u8>) -> String {
-    let mut passwd_hasher = Sha512::new();
-    passwd_hasher.update(password_hashes);
-    let result = passwd_hasher.finalize();
-    let result = format!("{:0x}", &result);
+    let digest = digest::digest(&digest::SHA512, password_hashes);
+    let digest_result: String = digest.as_ref().iter().map(|x| format!("{:02x}", &x)).collect();
     let mut password = cli_args.prefix.clone();
-    let used_hash = &result[..cli_args.length as usize];
+    let used_hash = &digest_result[..cli_args.length as usize];
     password.push_str(used_hash);
     password.push_str(&cli_args.suffix);
     password
